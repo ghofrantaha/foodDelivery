f22a2bd23bdcca6e4bf67f419aa11ef2cc4147d4
ghofrantaha <ghofrana2002@gmail.com>
2024-04-16 01:31:19 +0300

    some edits

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -23,58 +23,62 @@ class ProfilePage extends StatelessWidget {
     return Consumer<AuthController>(
       builder: (_, model, __) => Scaffold(
         appBar: AppBar(title: const Text('My Profile')),
-        body: ListView(
-          children: [
-            Container(
-              margin: const EdgeInsets.all(20),
-              child: Row(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.asset(
-                      'assets/profile_photo.jpeg',
-                      width: context.width * 0.25,
+        body: Container(
+          color: Colors.white,
+          child: ListView(
+            children: [
+              Container(
+                margin: const EdgeInsets.all(30),
+                child: Row(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.asset(
+                        'assets/profile_photo.jpeg',
+                        width: context.width * 0.25,
+                      ),
                     ),
-                  ),
-                  const SizedBox(width: 20),
-                  Column(
-                    children: [
-                      Text(
-                        'Ghufran Taha',
-                        style: Theme.of(context).textTheme.headlineMedium!,
-                      ),
-                      Text(
-                        model.auth.currentUser!.email!,
-                        style: Theme.of(context)
-                            .textTheme
-                            .headlineSmall!
-                            .copyWith(color: Colors.black45),
-                      ),
-                    ],
-                  )
-                ],
+                    const SizedBox(width: 20),
+                    Column(
+                      children: [
+                        Text(
+                          'Ghufran Taha',
+                          style: Theme.of(context).textTheme.headlineMedium!,
+                        ),
+                        Text(
+                          model.auth.currentUser!.email!,
+                          style: Theme.of(context)
+                              .textTheme
+                              .headlineSmall!
+                              .copyWith(color: Colors.black45),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
-            ),
-            const ProfileItemTile(
-                title: 'My Orders', content: 'Already have 12 orders'),
-            const ProfileItemTile(
-                title: 'Shipping Addresses', content: '3 addresses'),
-            const ProfileItemTile(
-                title: 'Payment Methods', content: 'Visa **43'),
-            const ProfileItemTile(
-                title: 'Promocodes', content: 'You have 2 special promocodes'),
-            const ProfileItemTile(
-                title: 'Settings', content: 'Notifications, Password, Help'),
-          ],
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20),
-          child: MainButton(
-              text: 'Log Out',
-              onTap: () {
-                _logout(model, context);
-              }),
+              const ProfileItemTile(
+                  title: 'My Orders', content: 'Already have 12 orders'),
+              const ProfileItemTile(
+                  title: 'Shipping Addresses', content: '3 addresses'),
+              const ProfileItemTile(
+                  title: 'Payment Methods', content: 'Visa **43'),
+              const ProfileItemTile(
+                  title: 'Promocodes',
+                  content: 'You have 2 special promocodes'),
+              const ProfileItemTile(
+                  title: 'Settings', content: 'Notifications, Password, Help'),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20),
+                child: MainButton(
+                    text: 'Log Out',
+                    onTap: () {
+                      _logout(model, context);
+                    }),
+              ),
+            ],
+          ),
         ),
       ),
     );
